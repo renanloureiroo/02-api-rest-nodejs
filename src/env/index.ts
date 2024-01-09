d83c384cf702b3b0432380794f2c4c93234bc372
Renan Pereira Loureiro <renan.pereiral@sonda.com>
2024-01-09 19:48:09 -0300

    chore: production setup

--- src/env/index.ts
+++ src/env/index.ts
@@ -14,8 +14,9 @@ const envSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'test', 'production'])
     .default('development'),
+  DATABASE_CLIENT: z.enum(['sqlite3', 'pg']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
